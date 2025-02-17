6900db5675f161d567145319e63844589dbe4756
emmanuel <emmanuelokorie67@gmail.com>
2025-02-17 15:23:06 +0100

    Ndeal Automated Commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,22 @@
+"use client"
 import Image from "next/image";
+import { CldVideoPlayer } from 'next-cloudinary';
+import 'next-cloudinary/dist/cld-video-player.css';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="absolute z-[-1] h-fit md:h-full w-full">
+        <CldVideoPlayer
+          loop={true}
+          autoplay={true}
+          muted={true}
+          controls={false}
+          width="1920"
+          height="1080"
+          src="xolemdfhyy690oxmbg5z"
+        />
+      </div>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
